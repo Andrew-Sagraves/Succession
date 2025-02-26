987db6d836636fa884fb9e1bec7507c59b9dd450
Andrew Sagraves <Andysagraves@gmail.com>
2025-02-25 22:02:24 -0500

    Commander introduced

--- entities/commander.go
+++ entities/commander.go
@@ -1,8 +1,27 @@
 package entities
 
 type Commander struct {
+	name       string
+	age        int
+	proficency int
 }
 
-func generate_commander(allegiance int) {
+func (c *Commander) Name() string {
+	return c.name
+}
 
+func (c *Commander) Age() int {
+	return c.age
+}
+
+func (c *Commander) Proficency() int {
+	return c.proficency
+}
+
+func Generate_commander() Commander {
+	var c Commander
+	c.name = ""
+	c.age = 25
+	c.proficency = 0
+	return c
 }
