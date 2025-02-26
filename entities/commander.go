310b93a579bf81b439c0cc2f777ca74dc9fa25b2
Andrew Sagraves <Andysagraves@gmail.com>
2025-02-26 14:01:24 -0500

    Commander augmented

--- entities/commander.go
+++ entities/commander.go
@@ -1,27 +1,40 @@
 package entities
 
+import (
+	"math/rand"
+)
+
+// struct for Commander type
 type Commander struct {
 	name       string
 	age        int
-	proficency int
+	attack     int
+	defence    int
+	open_field int
 }
 
 func (c *Commander) Name() string {
 	return c.name
 }
-
 func (c *Commander) Age() int {
 	return c.age
 }
-
-func (c *Commander) Proficency() int {
-	return c.proficency
+func (c *Commander) Attack() int {
+	return c.attack
+}
+func (c *Commander) Defence() int {
+	return c.defence
+}
+func (c *Commander) Open_fiend() int {
+	return c.open_field
 }
 
 func Generate_commander() Commander {
 	var c Commander
 	c.name = ""
 	c.age = 25
-	c.proficency = 0
+	c.attack = rand.Intn(10)
+	c.defence = rand.Intn(10)
+	c.open_field = rand.Intn(10)
 	return c
 }
