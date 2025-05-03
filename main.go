d70471410899d40546b6fc7dcd4852b44fc63378
dennymadapatu <dennismadapatu@gmail.com>
2025-05-03 16:26:46 -0400

    Starting to work with mouse input

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	// "log"
 	"cs302/final_project/Succession/server/board"
+	"os"
+	"os/exec"
 	// "github.com/gdamore/tcell/v2"
 	// "time"
 )
@@ -11,7 +13,38 @@ import (
 func main() {
 	var gameBoard board.Board = board.Generate_board() // Generating a board for gameplay usage
 	newScreen := board.SetColors(&gameBoard)
-	newScreen.Show()
+	// newScreen.Show()
+	cmd := exec.Command("x-terminal-emulator", "-e", "tail -f /tmp/output.log")
+	cmd.Start()
+	file, err := os.Create("/tmp/output.log")
+	if err != nil {
+		fmt.Println("Error creating log file:", err)
+		return
+	}
+	defer file.Close()
+	for {
+		newScreen.Show()
+		// newScreen.EnableMouse()
+		fmt.Println("Entered the loop")
+		mouse := newScreen.PollEvent()
+		switch mouse := mouse.(type) {
+		case *tcell.EventMouse:
+			x, y := mouse.Position()
+			buttons := mouse.Buttons()
+			_, err := fmt.Fprintf(file, "Mouse at (%d, %d) with buttons: %v\n", x, y, buttons)
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+			}
+			file.Sync()
+		case *tcell.EventKey:
+			if mouse.Key() == tcell.KeyEscape {
+				return
+			} else {
+				fmt.Fprintf(file, "Key clicked!")
+				file.Sync()
+			}
+		}
+	}
 
 	// this is all pseudocode, and feel free to put any testing code above, but this will outline the game
 	// loop for the game...
