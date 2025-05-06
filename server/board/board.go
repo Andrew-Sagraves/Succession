d6234e6798e1026d22fe46e26446c0fc27e47681
Andrew-Sagraves <andysagraves@gmail.com>
2025-05-06 16:00:18 -0400

    Update board.go

--- server/board/board.go
+++ server/board/board.go
@@ -6,8 +6,7 @@ type base [48][128]Cell
 // this struct has all information owned by the server
 type Board struct {
 	Base base
-	Batallions []Batallion
-	batallionMap map[[2]int]Batallion
+	Batallion Batallion
 	Cities   	[]City
 	cityMap  	map[[2]int]City
 	Villages 	[]Village
