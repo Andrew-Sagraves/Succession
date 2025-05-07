4dc64a8b564c632262a54b25450d62344cd91e63
Andrew-Sagraves <andysagraves@gmail.com>
2025-05-06 21:46:53 -0400

    Update batallion.go

--- server/board/batallion.go
+++ server/board/batallion.go
@@ -15,6 +15,24 @@ type Batallion struct {
 func (b *Batallion) X() int {
 	return b.x
 }
+func (b *Batallion) Y() int {
+	return b.y
+}
+
+func (b *Batallion) Left() {
+	b.y -= 1
+}
+func (b *Batallion) Right() {
+	b.y += 1
+}
+func (b *Batallion) Up() {
+	b.x += 1
+}
+
+func (b *Batallion) Down() {
+	b.x -= 1
+}
+
 func (b *Batallion) Troop_count() int {
 	return b.troop_count
 }
