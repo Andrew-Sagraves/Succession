f350da6fac68bd049bf5108dc187c518f0c51b1d
dennymadapatu <dennismadapatu@gmail.com>
2025-05-08 14:41:49 -0400

    Update setScreen.go

--- server/board/setScreen.go
+++ server/board/setScreen.go
@@ -38,32 +38,25 @@ func SetColors(b *Board) tcell.Screen {
 			if b.Base[i][j].Foundation == Water { // Water is blue
 				color := tcell.NewRGBColor(0, 0, 255)
 				screen.SetContent(j, i, ' ', nil, tcell.StyleDefault.Background(color))
-				screen.SetContent(j+1, i, ' ', nil, tcell.StyleDefault.Background(color))
 			} else {
 				if _, city := b.cityMap[currentLocation]; city { // Checks if a city exists in this specific vertex
 					color := tcell.NewRGBColor(0, 0, 0)
 					screen.SetContent(j, i, 'C', nil, tcell.StyleDefault.Background(color))
-					screen.SetContent(j+1, i, 'C', nil, tcell.StyleDefault.Background(color))
 				} else if _, village := b.villageMap[currentLocation]; village { // Checks if a village exists in this specific vertex
 					color := tcell.NewRGBColor(190, 100, 0)
 					screen.SetContent(j, i, 'V', nil, tcell.StyleDefault.Background(color))
-					screen.SetContent(j+1, i, 'V', nil, tcell.StyleDefault.Background(color))
-				} else if _, batallion := b.batallionMap[currentLocation]; batallion {
+				} else if _, batallion := b.batallionMap[currentLocation]; batallion { // Checks if a batallion exists in this specific vertex
 					color := tcell.NewRGBColor(0, 0, 128)
 					screen.SetContent(j, i, 'B', nil, tcell.StyleDefault.Background(color))
-					screen.SetContent(j+1, i, 'B', nil, tcell.StyleDefault.Background(color))
-				}else if b.Base[i][j].Biome == Agriculture {
+				}else if b.Base[i][j].Biome == Agriculture { // This section is to make the map look a bit more colorful
 					color := tcell.NewRGBColor(144, 238, 144)
 					screen.SetContent(j, i, '*', nil, tcell.StyleDefault.Background(color))
-					screen.SetContent(j+1, i, '*', nil, tcell.StyleDefault.Background(color))
 				} else if b.Base[i][j].Biome == Field {
 					color := tcell.NewRGBColor(83, 128, 89)
 					screen.SetContent(j, i, ':', nil, tcell.StyleDefault.Background(color))
-					screen.SetContent(j+1, i, ':', nil, tcell.StyleDefault.Background(color))
 				} else if b.Base[i][j].Biome == Woods {
 					color := tcell.NewRGBColor(63, 90, 54)
 					screen.SetContent(j, i, '^', nil, tcell.StyleDefault.Background(color))
-					screen.SetContent(j+1, i, '^', nil, tcell.StyleDefault.Background(color))
 				}
 			}
 			// Need to make a priority list for the different types of cells
@@ -72,15 +65,30 @@ func SetColors(b *Board) tcell.Screen {
 	return screen
 }
 
-func returnCityMap(b *Board) map[[2]int]City {
-	return b.cityMap
-} 
-func returnVillageMap(b *Board) map[[2]int]Village {
-	return b.villageMap
-} 
-
-// Steps:
-// Step 1: Take in the game board, and edit all of the game board indices with a color
-//		- Go basic first, just make colorBoard land and water
-//		- Assign specific colors to specific tiles
-//		- Make biomes different shades
+// func UpdateColors(b *Board, screen tcell.Screen, previousX int, previousY int, currentX int, currentY int) tcell.Screen { A function meant to lower time taken to take input and display the change
+// 	if b.Base[previousX][previousY].Foundation == Water { // Water is blue
+// 		color := tcell.NewRGBColor(0, 0, 255)
+// 		screen.SetContent(previousY, previousX, ' ', nil, tcell.StyleDefault.Background(color))
+// 	} else {
+// 		previousLocation := [2]int {previousX, previousY}
+// 		if _, city := b.cityMap[previousLocation]; city { // Checks if a city exists in this specific vertex
+// 			color := tcell.NewRGBColor(0, 0, 128) // Changes colors of the city to show the batallion has taken it
+// 			screen.SetContent(previousY, previousX, 'C', nil, tcell.StyleDefault.Background(color))
+// 		} else if _, village := b.villageMap[previousLocation]; village { // Checks if a village exists in this specific vertex
+// 			color := tcell.NewRGBColor(0, 0, 128) // Changes colors of the city to show the batallion has taken it
+// 			screen.SetContent(previousY, previousX, 'V', nil, tcell.StyleDefault.Background(color))
+// 		}else if b.Base[previousX][previousY].Biome == Agriculture { // This section is to make the map look a bit more colorful
+// 			color := tcell.NewRGBColor(144, 238, 144)
+// 			screen.SetContent(previousY, previousX, '*', nil, tcell.StyleDefault.Background(color))
+// 		} else if b.Base[previousX][previousY].Biome == Field {
+// 			color := tcell.NewRGBColor(83, 128, 89)
+// 			screen.SetContent(previousY, previousX, ':', nil, tcell.StyleDefault.Background(color))
+// 		} else if b.Base[previousY][previousX].Biome == Woods {
+// 			color := tcell.NewRGBColor(63, 90, 54)
+// 			screen.SetContent(previousY, previousX, '^', nil, tcell.StyleDefault.Background(color))
+// 		}
+// 	}
+// 	color := tcell.NewRGBColor(0,0,128) 
+// 	screen.SetContent(currentY, currentX, 'B', nil, tcell.StyleDefault.Background(color)) This is to make the new cell the batallion cell
+// 	return screen
+// }
